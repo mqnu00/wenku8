6e47ee5c99ec6e2f6a3e4b21771e7161f7592696
廖泽晖 <647983952@qq.com>
2024-04-01 19:53:50 +0800

    小说信息添加目录和链接

--- wenku8/novel/data.py
+++ wenku8/novel/data.py
@@ -1,1 +1,3 @@
 info_path = '/book/{}.htm'
+catalog_path = '/novel/1/{}/index.htm'
+chapter_path = '/novel/1/{}/{}'
--- wenku8/novel/novel.py
+++ wenku8/novel/novel.py
@@ -11,6 +11,7 @@ class Novel:
     author = str()
     description = str()
     pic_url = str()
+    catalog = dict()
 
     def __init__(self):
         pass
@@ -55,15 +56,58 @@ class Novel:
         pic_url = pic_url[pic_url.find("src") + 5:pic_url.find("jpg") + 3]
         self.pic_url = pic_url
 
+        # 获取小说目录
+        self.get_catalog()
+
         res["status"] = True
         res["info"] = "获取小说信息成功"
         res["novel"] = json.dumps(self.__dict__, ensure_ascii=False)
 
         return res
 
+    # 获取小说目录
+    def get_catalog(self) -> None:
+        catalog = {
+            "volume": str(),
+            "chapter": list()
+        }
+        chapter = {
+            "name": str(),
+            "link": str()
+        }
+        res = list()
+        catalog_url = wenku8.data.url + data.catalog_path
+        catalog_url = catalog_url.format(self.id)
+        response = requests.get(
+            url=catalog_url,
+            headers=wenku8.data.header,
+            timeout=10
+        )
+        soup = BeautifulSoup(response.content, 'html.parser')
+        content = soup.find_all('td')
+        for i in content:
+            if str(i).find('colspan') != -1:
+                if catalog["volume"] != '':
+                    res.append(catalog)
+                    catalog = {
+                        "volume": str(),
+                        "chapter": list()
+                    }
+                catalog["volume"] = i.text
+
+            elif i.a:
+                chapter = {
+                    "name": i.text,
+                    "link": wenku8.data.url + data.chapter_path.format(self.id, i.a.get('href'))
+                }
+                catalog["chapter"].append(chapter)
+            else:
+                pass
+        res.append(catalog)
+        self.catalog = res
+
 
 if __name__ == '__main__':
-
     novel = Novel()
     novel.id = 1973
     print(novel.get_info())
